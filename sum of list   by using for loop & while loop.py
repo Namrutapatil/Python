8c648dbb408bdca5d234199e88c0607408810bf5
Namrutapatil <patilnamruta30@gmail.com>
2024-10-03 18:33:02 -0800

    Add files via upload

---
+++ sum of list   by using for loop & while loop.py
@@ -0,0 +1,16 @@
+#list=(5,10,15,20,25,30)
+#sum=0
+#i=0
+#while i<len(list):
+   # print(list[i])
+  #  sum+=list[i]
+ #   i+=1
+#print("sum",sum)
+
+list=(5,10,15,20,25,30)
+sum=0
+i=0
+for i in list:
+    print(i)
+    sum+=i
+print("sum",sum)
